fdbbe1b7e75758a8affb591f8475771946ebf582
AndresLli17 <andreslli1707@gmail.com>
2019-06-23 23:27:25 -0500

    Dame la formulosky

--- src/modelo/Token.java
+++ src/modelo/Token.java
@@ -20,6 +20,54 @@ public class Token {
 		return "Token [categoria=" + categoria + ", palabra=" + palabra + ", fila=" + fila + ", columna=" + columna
 				+ "]";
 	}
+	/**
+	 * @return the categoria
+	 */
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	/**
+	 * @param categoria the categoria to set
+	 */
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	/**
+	 * @return the palabra
+	 */
+	public String getPalabra() {
+		return palabra;
+	}
+	/**
+	 * @param palabra the palabra to set
+	 */
+	public void setPalabra(String palabra) {
+		this.palabra = palabra;
+	}
+	/**
+	 * @return the fila
+	 */
+	public int getFila() {
+		return fila;
+	}
+	/**
+	 * @param fila the fila to set
+	 */
+	public void setFila(int fila) {
+		this.fila = fila;
+	}
+	/**
+	 * @return the columna
+	 */
+	public int getColumna() {
+		return columna;
+	}
+	/**
+	 * @param columna the columna to set
+	 */
+	public void setColumna(int columna) {
+		this.columna = columna;
+	}
 	
 	
 
