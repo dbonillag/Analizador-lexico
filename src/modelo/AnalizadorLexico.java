8e724faf805d31eb9072f2adb5f3aee7e6c76936
Daniel <dbonillag_1@uqvirtual.edu.co>
2019-09-12 17:10:35 -0500

    Agregados requisitos de compiladores, falta mostrar errores lexicos

--- src/modelo/AnalizadorLexico.java
+++ src/modelo/AnalizadorLexico.java
@@ -9,12 +9,14 @@ public class AnalizadorLexico {
 
 	private String codigoFuente;
 	private ArrayList<Token> listaTokens;
+	private ArrayList<ErrorLexico> listaErrores;
 	private char caracterActual, finCodigo;
 	private int posicionActual, columnaActual, filaActual;
 
 	public AnalizadorLexico(String codigoFuente) {
 		this.codigoFuente = codigoFuente;
 		this.listaTokens = new ArrayList<>();
+		this.listaErrores=new ArrayList<>();
 		this.caracterActual = codigoFuente.charAt(0);
 		this.finCodigo = '°';
 	}
@@ -53,16 +55,25 @@ public class AnalizadorLexico {
 				continue;
 			if (esLlaveCierre())
 				continue;
+			if (esCorcheteApertura())
+				continue;
+			if (esCorcheteCierre())
+				continue;
+			if (esPunto())
+				continue;
+			if (esDosPuntos())
+				continue;
 			if (esTerminal())
 				continue;
 			if (esSeparador())
 				continue;
-			if (esHexadecimal())
+			if (esComentarioLinea())
+				continue;
+			if (esComentarioBloque())
 				continue;
 			if (esCadenaDeCaracteres())
 				continue;
-			if (esComentario())
-				continue;
+		
 
 			listaTokens.add(new Token(Categoria.DESCONOCIDO, "" + caracterActual, filaActual, columnaActual));
 			obtenerSiguienteCaracter();
@@ -87,7 +98,7 @@ public class AnalizadorLexico {
 
 			}
 
-			if (caracterActual == '.'||caracterActual == 'h') {
+			if (caracterActual == '.') {
 				// backTracking
 				return backTracking(palabra, fila, columna);
 			} else {
@@ -172,21 +183,23 @@ public class AnalizadorLexico {
 			palabra += caracterActual;
 			obtenerSiguienteCaracter();
 
-			if (Character.isLetter(caracterActual) || caracterActual == '_') {
+			if (Character.isLetter(caracterActual)) {
 				palabra += caracterActual;
 				obtenerSiguienteCaracter();
-
-				while (Character.isLetter(caracterActual) || caracterActual == '_') {
+				
+				while (Character.isLetter(caracterActual)) {
 					palabra += caracterActual;
 					obtenerSiguienteCaracter();
 				}
 
-				listaTokens.add(new Token(Categoria.IDENTIFICADOR, palabra, fila, columna));
-				return true;
+				
 
 			} else {
-				return backTracking(palabra, fila, columna);
+				listaErrores.add(new ErrorLexico("El identificador debe tener al menos una letra", fila, columna));
 			}
+			
+			listaTokens.add(new Token(Categoria.IDENTIFICADOR, palabra, fila, columna));
+			return true;
 
 		}
 
@@ -364,13 +377,13 @@ public class AnalizadorLexico {
 				palabra += caracterActual;
 				obtenerSiguienteCaracter();
 				
-				listaTokens.add(new Token(Categoria.ASIGNACION, palabra, fila, columna));
-				return true;
-				
 			}else {
-				return backTracking(palabra, fila, columna);
+				listaErrores.add(new ErrorLexico("Operador de asignación necesita un = para estar completo", fila, columna));
+				
 			}
-
+			
+			listaTokens.add(new Token(Categoria.ASIGNACION, palabra, fila, columna));
+			return true;
 			
 		}
 
@@ -500,7 +513,82 @@ public class AnalizadorLexico {
 		return false;
 	}
 	
+	public boolean esCorcheteApertura() {
+		if (caracterActual == '[') {
+			String palabra = "";
+			int fila = filaActual;
+			int columna = columnaActual;
+
+			// transicion
+			palabra += caracterActual;
+			obtenerSiguienteCaracter();
+			
+			listaTokens.add(new Token(Categoria.CORCHETE_APERTURA, palabra, fila, columna));
+			return true;
+			
+		}
+
+		// rechazo inmediato
+		return false;
+	}
 	
+	
+	public boolean esCorcheteCierre() {
+		if (caracterActual == '}') {
+			String palabra = "";
+			int fila = filaActual;
+			int columna = columnaActual;
+
+			// transicion
+			palabra += caracterActual;
+			obtenerSiguienteCaracter();
+			
+			listaTokens.add(new Token(Categoria.CORCHETE_CIERRE, palabra, fila, columna));
+			return true;
+			
+		}
+
+		// rechazo inmediato
+		return false;
+	}
+	
+	public boolean esPunto() {
+		if (caracterActual == '.') {
+			String palabra = "";
+			int fila = filaActual;
+			int columna = columnaActual;
+
+			// transicion
+			palabra += caracterActual;
+			obtenerSiguienteCaracter();
+			
+			listaTokens.add(new Token(Categoria.PUNTO, palabra, fila, columna));
+			return true;
+			
+		}
+
+		// rechazo inmediato
+		return false;
+	}
+	
+	public boolean esDosPuntos() {
+		if (caracterActual == ':') {
+			String palabra = "";
+			int fila = filaActual;
+			int columna = columnaActual;
+
+			// transicion
+			palabra += caracterActual;
+			obtenerSiguienteCaracter();
+			
+			listaTokens.add(new Token(Categoria.DOS_PUNTOS, palabra, fila, columna));
+			return true;
+			
+		}
+
+		// rechazo inmediato
+		return false;
+	}
 	
 	public boolean esTerminal() {
 		if (caracterActual == '!') {
@@ -540,6 +628,124 @@ public class AnalizadorLexico {
 		return false;
 	}
 	
+	public boolean esComentarioLinea() {
+		if (caracterActual=='¿') {
+			String palabra = "";
+			int fila = filaActual;
+			int columna = columnaActual;
+	
+			// transicion
+			palabra += caracterActual;
+			obtenerSiguienteCaracter();
+			
+			if(caracterActual=='¿') {
+				backTracking(palabra, fila, columna);
+			}else {
+				while (caracterActual!='\n'&&caracterActual!=finCodigo) {
+					palabra += caracterActual;
+					obtenerSiguienteCaracter();
+		
+				}
+				
+				listaTokens.add(new Token(Categoria.COMENTARIO_LINEA, palabra, fila, columna));
+				return true;
+			}	
+		}
+		// rechazo inmediato
+		return false;
+	}
+	
+	public boolean esComentarioBloque() {
+		if (caracterActual=='¿') {
+			String palabra = "";
+			int fila = filaActual;
+			int columna = columnaActual;
+	
+			// transicion
+			palabra += caracterActual;
+			obtenerSiguienteCaracter();
+	
+			if(caracterActual=='¿') {
+				
+				
+				while (caracterActual!=finCodigo) {
+					palabra += caracterActual;
+					obtenerSiguienteCaracter();
+					if (caracterActual=='?') {
+						palabra += caracterActual;
+						obtenerSiguienteCaracter();
+						if (caracterActual=='?') {
+							palabra += caracterActual;
+							obtenerSiguienteCaracter();
+							
+						}
+						
+					}
+		
+				}
+				
+				listaErrores.add(new ErrorLexico("Comentario de bloque no ha sido cerrado", fila, columna));
+				listaTokens.add(new Token(Categoria.COMENTARIO_BLOQUE, palabra, fila, columna));
+				return true;
+			}else {
+				return backTracking(palabra, fila, columna);
+			}
+			
+			
+		}
+		// rechazo inmediato
+		return false;
+	}
+
+	public boolean esCadenaDeCaracteres() {
+		if (caracterActual=='"') {
+			String palabra = "";
+			int fila = filaActual;
+			int columna = columnaActual;
+	
+			// transicion
+			palabra += caracterActual;
+			obtenerSiguienteCaracter();
+	
+			
+			
+			while (caracterActual!='"'&&caracterActual!=finCodigo&&caracterActual!='\n') {
+				if (caracterActual=='\\') {
+					palabra += caracterActual;
+					obtenerSiguienteCaracter();
+					if (caracterActual=='\''||caracterActual=='"'||caracterActual=='\\'||caracterActual=='t'||caracterActual=='b'||caracterActual=='r'||caracterActual=='f'||caracterActual=='n') {
+						palabra += caracterActual;
+						obtenerSiguienteCaracter();
+					}else if(caracterActual=='\'') {
+						
+						
+						listaErrores.add(new ErrorLexico("caracter despues del backslash invalido", fila, columna));
+						
+					}
+				}else {
+					
+					palabra += caracterActual;
+					obtenerSiguienteCaracter();
+					
+				}		
+			}
+			
+			if (caracterActual == '"') {
+				palabra += caracterActual;
+				obtenerSiguienteCaracter();
+				
+				
+			}else{
+				// backTracking
+				listaErrores.add(new ErrorLexico("Falta \" para finalizar la cadena", fila, columna));
+			}
+			listaTokens.add(new Token(Categoria.CADENA, palabra, fila, columna));
+			return true;
+		}
+		// rechazo inmediato
+		return false;
+	}
+
 	private boolean backTracking(String palabra, int fila, int columna) {
 		this.filaActual = fila;
 		this.columnaActual = columna;
@@ -564,115 +770,8 @@ public class AnalizadorLexico {
 
 	}
 	
-	public boolean esHexadecimal() {
-		if (Character.isDigit(caracterActual)||(caracterActual>='A'&&caracterActual<='F')) {
-			String palabra = "";
-			int fila = filaActual;
-			int columna = columnaActual;
-
-			// transicion
-			palabra += caracterActual;
-			obtenerSiguienteCaracter();
-
-			while (Character.isDigit(caracterActual)||(caracterActual>='A'&&caracterActual<='F')) {
-				palabra += caracterActual;
-				obtenerSiguienteCaracter();
-
-			}
-
-			if (caracterActual == 'h') {
-				palabra += caracterActual;
-				obtenerSiguienteCaracter();
-				
-				listaTokens.add(new Token(Categoria.HEXADECIMAL, palabra, fila, columna));
-				return true;
-				
-			} else {
-				// backTracking
-				return backTracking(palabra, fila, columna);
-			}
-		}
-		// rechazo inmediato
-		return false;
-	}
-
-	public boolean esCadenaDeCaracteres() {
-		if (caracterActual=='"') {
-			String palabra = "";
-			int fila = filaActual;
-			int columna = columnaActual;
-
-			// transicion
-			palabra += caracterActual;
-			obtenerSiguienteCaracter();
-
-			
-			
-			while (caracterActual!='"'||caracterActual!='\''||caracterActual!='\n') {
-				if (caracterActual=='\\') {
-					palabra += caracterActual;
-					obtenerSiguienteCaracter();
-					if (caracterActual=='\''||caracterActual=='"'||caracterActual=='\\'||caracterActual=='t'||caracterActual=='b'||caracterActual=='r'||caracterActual=='f'||caracterActual=='n') {
-						palabra += caracterActual;
-						obtenerSiguienteCaracter();
-					}else {
-						return backTracking(palabra, fila, columna);
-					}
-				}else {
-					
-					palabra += caracterActual;
-					obtenerSiguienteCaracter();
-					
-				}		
-			}
-			
-			if (caracterActual == '"') {
-				palabra += caracterActual;
-				obtenerSiguienteCaracter();
-				
-				listaTokens.add(new Token(Categoria.CADENA, palabra, fila, columna));
-				return true;
-				
-			} else {
-				// backTracking
-				return backTracking(palabra, fila, columna);
-			}
-		}
-		// rechazo inmediato
-		return false;
-	}
 	
-	
-	public boolean esComentario() {
-		if (caracterActual=='¿') {
-			String palabra = "";
-			int fila = filaActual;
-			int columna = columnaActual;
 
-			// transicion
-			palabra += caracterActual;
-			obtenerSiguienteCaracter();
-
-			while (caracterActual!='?'||caracterActual!='°') {
-				palabra += caracterActual;
-				obtenerSiguienteCaracter();
-
-			}
-			if (caracterActual=='?') {
-				palabra += caracterActual;
-				obtenerSiguienteCaracter();
-				
-				listaTokens.add(new Token(Categoria.HEXADECIMAL, palabra, fila, columna));
-				return true;
-			}else {
-				return backTracking(palabra, fila, columna);
-			}
-			
-		}
-		// rechazo inmediato
-		return false;
-	}
-	
 	/**
 	 * @return the listaTokens
 	 */
